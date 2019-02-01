ceada7b1acb455b6729abed71a48c4e83f3d0882
FrozenHunter <47252052+FrozenHunter@users.noreply.github.com>
2019-02-01 18:09:10 +0100

    Reupdate

--- JavaProject/src/ex1/Class1.java
+++ JavaProject/src/ex1/Class1.java
@@ -1,5 +1,11 @@
 package ex1;
 
 public class Class1 {
+	public static void afficherBonjour() {
+		System.out.println("Bonjour");
+	}
 
+	public static void main(String args[]) {
+		afficherBonjour();
+	}
 }
