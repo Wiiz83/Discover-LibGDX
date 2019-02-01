1bae2d88b7505da9c5da6303e8c12cdd6646d8af
Lucas <uzanlah@gmail.com>
2019-02-01 18:07:30 +0100

    ArrayList Main

---
+++ JavaProject/src/ex1/Class2.java
@@ -0,0 +1,16 @@
+package ex1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Class2 {
+	
+	static List<String> maListeChaine;
+	
+	public static void main(String args[]){
+		maListeChaine = new ArrayList<String>();
+		System.out.println("");
+		
+	}
+
+}
