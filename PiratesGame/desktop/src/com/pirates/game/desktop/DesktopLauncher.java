add0db66b1616ff4afdc16771fef46a56b5f5ed5
FrozenHunter <47252052+FrozenHunter@users.noreply.github.com>
2019-02-22 18:21:43 +0100

    creaProjet

--- PiratesGame/desktop/src/com/pirates/game/desktop/DesktopLauncher.java
+++ PiratesGame/desktop/src/com/pirates/game/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.pirates.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.pirates.game.LauchGame;
+import com.pirates.game.LaunchGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new LauchGame(), config);
+		new LwjglApplication(new LaunchGame(), config);
 	}
 }
