398eceb204ee6f842b75a607cb4bcf60e9c9d22d
FrozenHunter <47252052+FrozenHunter@users.noreply.github.com>
2019-02-22 18:46:29 +0100

    jt

--- PiratesGame/desktop/src/com/pirates/game/desktop/DesktopLauncher.java
+++ PiratesGame/desktop/src/com/pirates/game/desktop/DesktopLauncher.java
@@ -5,6 +5,24 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.pirates.game.LaunchGame;
 
 public class DesktopLauncher {
+	public class Personnage
+	{
+		public int m_vie;
+	}
+	public void setup()
+	{
+		boolean isRunning = true;
+		Personnage a  = new Personnage();
+		
+	}
+	public static void input()
+	{
+	
+	}
+	public static void draw()
+	{
+		
+	}
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new LaunchGame(), config);
